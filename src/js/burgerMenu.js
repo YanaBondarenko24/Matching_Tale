cf188f2e7616c1fc8b5a6d72c85c5d607c3190fa
Yana Bondarenko <yana-lyana27@mail.ru>
2026-06-07 14:32:36 +0200

    add how-to-play section

--- src/js/burgerMenu.js
+++ src/js/burgerMenu.js
@@ -2,12 +2,27 @@ const openBtn = document.querySelector('[data-action="open"]');
 const closeBtn = document.querySelector('[data-action="close"]');
 const burgerMenu = document.querySelector('[data-visible]');
 const body = document.querySelector('[data-menu="close"]')
-openBtn.addEventListener('click', e => {
+const list = document.querySelector('[data-list]');
+
+list.addEventListener("click", handleClick);
+openBtn.addEventListener("click", openMenu);
+closeBtn.addEventListener("click", closeMenu);
+
+function handleClick(e) {
+  if (e.target.closest('[data-item]')) {
+    closeMenu();
+  }
+  
+}
+
+function openMenu (e){
   burgerMenu.dataset.visible = 'open';
   body.dataset.menu = 'open';
-});
+};
 
-closeBtn.addEventListener('click', e => {
+function closeMenu(e){
   burgerMenu.dataset.visible = 'close';
   body.dataset.menu = 'close';
-});
+};
+
+
